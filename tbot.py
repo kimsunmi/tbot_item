84cddec9db3c785b3770a2d2ce3cd707bbede2aa
kimsunmi <42822870+kimsunmi@users.noreply.github.com>
2020-09-06 01:32:23 +0900

    Update tbot.py

--- tbot.py
+++ tbot.py
@@ -2,19 +2,17 @@ import discord
 import os
 import asyncio
 
-
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    await client.change_presence(game=discord.Game(name="itemShop",type=1))
+    #await client.change_presence(game=discord.Game(name="itemShop",type=1))
     print("this is item shop. welcome!")
 
 @client.event
 async def on_message(message):
-    id = message.author.id
+    #id = message.author.id
     if message.content.startswith('!itemlist'):
-        await client.send_message('Ha ha, What do you want?')
-    await message.channel.send()
+        await message.channel.send('Ha ha, What do you want?')
 
 client.run(os.environ['token'])
