9328f63a70ece57e16892e0a9e1da2e8e1359bbc
kimsunmi <42822870+kimsunmi@users.noreply.github.com>
2020-09-08 15:26:10 +0900

    itemlist출력(수정필요)

--- itemlists.py
+++ itemlists.py
@@ -1,5 +1,27 @@
 import pymysql
 
+def sql_update(query, *args):
+    db_conn = pymysql.connect(
+        user='staff', 
+        passwd='algoalgo-staff', 
+        host='34.64.120.154', 
+        db='algoalgo', 
+        charset='utf8'
+    )   
+
+    cursor = db_conn.cursor(pymysql.cursors.DictCursor)
+    try:
+        if args == None:
+            cursor.execute(query)
+        else:
+            cursor.execute(query, args)
+
+        db_conn.commit()
+        db_conn.close()
+    
+    except Exception as ex:
+        raise ex
+
 def sql_exe(query):
     db_conn = pymysql.connect(
         user='staff', 
@@ -29,8 +51,7 @@ def useitem(author):
     try:
         sql_result=sql_exe(sql)
         # 인덱스. 아이템명 : 소유 개수 형식의 리스트 출력해야함
-        return "[*] success print item {author}", sql_result
+        return "[*] success print itemlist {author}", sql_result
     except Exception as ex:
         return "[!] error finding your info: {ex}"
 
-    
