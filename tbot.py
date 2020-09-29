b56cc42ab2e6631758a12436a219a1e13f971637
kimsunmi <42822870+kimsunmi@users.noreply.github.com>
2020-09-30 03:22:24 +0900

    Update tbot.py

--- tbot.py
+++ tbot.py
@@ -89,7 +89,7 @@ async def on_message(message):
                 await message.channel.send(embed=embed)
                 return
 
-            user_res2 = result[user_res][0] # user_res2 = 아이템 명
+            user_res2 = result[user_res] # user_res2 = 아이템 명
             await message.channel.send(user_res2)
             if user_res2 == 'STUN':
                 # 상대방 status = -1 로 업데이트
